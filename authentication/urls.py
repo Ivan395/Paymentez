9432c7b425fc05a55c9ef776e09e8765ad9cb442
Angel Ivan Carrillo de Leon <angelivan395@gmail.com>
2021-10-03 17:55:18 -0500

    feat: Add create and list path to urlpatterns for consume endpoints

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from authentication.views import CreateUserView, GetAllUsersView
+
+
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/create/', CreateUserView.as_view(), name='user_create'),
+    path('user/list/', GetAllUsersView.as_view(), name='user_list'),
 ]
